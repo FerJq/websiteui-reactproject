dff0b12d41fd15da7d18b77e80762fffc3f28c0d
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-04-24 00:02:32 -0400

    Update WhatGPT3.jsx

--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./whatgpt3.css";
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+    </div>
+  );
 };
 export default WhatGPT3;
